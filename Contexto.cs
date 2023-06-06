a6da1cf03ff1a09f130adff29b87e5e4365e0756
Bruno Mota <bmotadev@gmail.com>
2023-06-06 16:44:18 -0300

    Add project files.

---
+++ Contexto.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleAppEntity.DataModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEntity
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+        public Contexto()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["EntityAtos"];
+
+            string retorno = "";
+
+            if(settings != null)
+            {
+                retorno = settings.ConnectionString;
+            }
+
+            optionsBuilder.UseSqlServer(retorno);
+
+            optionsBuilder.UseLazyLoadingProxies();// se eu tenho um atributo de uma outra classe. Ele tras atributos que tem conexão entre eles.
+
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Email>()
+                .HasOne(e => e.pessoa)
+                .WithMany(e => e.Emails)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
